95112adca33292878dd634f13f46fc8d9e053132
Deepak Saini <deepak1307saini@gmail.com>
2023-02-17 17:49:47 +0530

    User file  added

---
+++ src/User.java
@@ -0,0 +1,36 @@
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class User {
+    //All final attributes
+
+    private Long Id;
+    private String name; // required
+    private int age; // optional
+    private String phone; // optional
+    private String address; // optional
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+}
